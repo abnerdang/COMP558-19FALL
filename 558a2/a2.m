78f107587565b43cf34dd8c84975981ace581843
bodang <bdang@foxmail.com>
2019-11-05 00:43:35 -0500

    a2 q3 update

--- 558a2/a2.m
+++ 558a2/a2.m
@@ -1,4 +1,6 @@
 %read the image, take the blue channel
+clear all;
+clc;
 image = imread('manor.png');
 original_image = image(:, :, 3);
 
@@ -39,7 +41,8 @@ for i = 1:6
 end
     
 % ====================Find SIFT features============================
-
+keypoint = [];
+th = 15;
 spatial_size = 3;
 for l_num = 2:5
     current = lplpyramid{l_num};
@@ -50,36 +53,55 @@ for l_num = 2:5
             [current_min, current_max] = findCurrLocalExtremas(spatial_size, i, j, current);
             [next_min, next_max] = getLocalExtremas(spatial_size, i, j, next);
             [previous_min, previous_max] = getLocalExtremas(spatial_size, i, j, previous);
-  
+            if current(i, j) > current_max+th && current(i, j) > next_max+th && current(i, j) > previous_max+th
+                tmp = [i, j, 2^(l_num-1)];
+                keypoint = [keypoint; tmp];
+            end
+            if current(i, j) < current_min-th && current(i, j) < next_min-th && current(i, j) < previous_min-th
+                tmp = [i, j, 2^(l_num-1)];
+                keypoint = [keypoint; tmp];
+            end
         end
     end
+end
+
+color = {'r', 'g', 'b'};
+figure(3);
+imshow(original_image);
+hold on;
+for n = 1:size(keypoint, 1)
+    radius = keypoint(n, 3);
+    coordinate_tmp = [keypoint(n, 1), keypoint(n, 2)];
+    level = log2(keypoint(n, 3));
+    viscircles(coordinate_tmp.*radius, radius, 'LineWidth', 0.5, 'Color', 'r');
 end
 
 
 
 
+% =========================Utils funciton=================================
 function [minv, maxv] = findCurrLocalExtremas(N, x, y, image)
     minv = 999;
     maxv = -999;
     shift = (N-1)/2;
     for i = x-shift:x+shift
         for j = y-shift:y+shift
-            if i ~= x && j ~= y:
+            if i ~= x && j ~= y
                 if image(i, j) < minv
-                    minv = image(i, j)
+                    minv = image(i, j);
                 end
                 if image(i, j) > maxv
-                    maxv = image(i, j)
+                    maxv = image(i, j);
                 end
             end
         end
     end       
 end
 
-function [minv, maxv] = getLocalExtremas(N, image, i, j)
+function [minv, maxv] = getLocalExtremas(N, i, j, image)
     shift = (N-1)/2;
-    maxv = max(max(image(i-shift:i+shift, j-shift, j+shift)));
-    minv = min(min(image(i-shift:i+shift, j-shift, j+shift)));
+    maxv = max(max(image(i-shift:i+shift, j-shift:j+shift)));
+    minv = min(min(image(i-shift:i+shift, j-shift:j+shift)));
 end
             
 
