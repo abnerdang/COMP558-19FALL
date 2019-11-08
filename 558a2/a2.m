a5680c1abf05a3eadea9065bc2fea30d419c8495
bodang <bdang@foxmail.com>
2019-11-08 02:00:41 -0500

    q4 q5 q6 update

--- 558a2/a2.m
+++ 558a2/a2.m
@@ -2,7 +2,8 @@
 clear all;
 clc;
 image = imread('manor.png');
-original_image = image(:, :, 3);
+original_image = rgb2gray(image);
+% original_image = image(:, :, 3);
 
 %====================create Gaussian pyramid============================
 gaupyramid = cell(7, 1);
@@ -34,30 +35,36 @@ for i = 1:6
     low = gaupyramid{i};
     high = gaupyramid{i+1};
     high = imresize(high, 2);
-    lplpyramid{i} = high - low + 128;
+    lplpyramid{i} = high - low;
     
     subplot(2, 3, i);
-    imshow(uint8(lplpyramid{i}));
+    imshow(uint8(lplpyramid{i}), []);
 end
     
 % ====================Find SIFT features============================
 keypoint = [];
-th = 15;
+th = 5;
 spatial_size = 3;
+shift = (spatial_size-1)/2;
+sift_window = zeros(spatial_size*spatial_size, spatial_size);
 for l_num = 2:5
     current = lplpyramid{l_num};
-    previous = imresize(lplpyramid{l_num-1}, size(current));
-    next = imresize(lplpyramid{l_num+1}, size(current));
+    previous = imresize(lplpyramid{l_num-1}, 0.5);
+    next = imresize(lplpyramid{l_num+1}, 2);
     for i = 2:size(current, 1)-1
         for j = 2:size(current, 2)-1
-            [current_min, current_max] = findCurrLocalExtremas(spatial_size, i, j, current);
-            [next_min, next_max] = getLocalExtremas(spatial_size, i, j, next);
-            [previous_min, previous_max] = getLocalExtremas(spatial_size, i, j, previous);
-            if current(i, j) > current_max+th && current(i, j) > next_max+th && current(i, j) > previous_max+th
+            sift_window(1:spatial_size, 1:spatial_size) = current(i-shift:i+shift, j-shift:j+shift);
+            sift_window(spatial_size+1:(spatial_size*2), 1:spatial_size) = previous(i-shift:i+shift, j-shift:j+shift);
+            sift_window((spatial_size*2+1):spatial_size*3, 1:spatial_size) = next(i-shift:i+shift, j-shift:j+shift);
+            sift_window(2, 2) = sift_window(1, 1);
+            maxv = max(max(sift_window));
+            minv = min(min(sift_window));
+            if current(i, j) > (maxv+th)
                 tmp = [i, j, 2^(l_num-1)];
                 keypoint = [keypoint; tmp];
             end
-            if current(i, j) < current_min-th && current(i, j) < next_min-th && current(i, j) < previous_min-th
+            %min_v = min([current_min, next_min, previous_min]);
+            if current(i, j) < (minv-th)
                 tmp = [i, j, 2^(l_num-1)];
                 keypoint = [keypoint; tmp];
             end
@@ -71,12 +78,13 @@ imshow(original_image);
 hold on;
 for n = 1:size(keypoint, 1)
     radius = keypoint(n, 3);
-    coordinate_tmp = [keypoint(n, 1), keypoint(n, 2)];
+    coordinate_tmp = [keypoint(n, 2), keypoint(n, 1)];
     level = log2(keypoint(n, 3));
     viscircles(coordinate_tmp.*radius, radius, 'LineWidth', 0.5, 'Color', color{level});
 end
 
 % ====================Q4. Compute SIFT feature vectors=====================
+selected_keypoint_index = 152;
 gmag = cell(7, 1);
 gdir = cell(7, 1);
 gau_gmag = cell(7, 1);
@@ -87,14 +95,18 @@ for i = 1:7
     [x_gmag{i}, y_gmag{i}] = imgradientxy(gaupyramid{i});
 end
 % choose a keypoint
-selected_point = keypoint(51, :);
+selected_point = keypoint(selected_keypoint_index, :);
 x = selected_point(1);
 y = selected_point(2);
 level = log2(selected_point(3))+1;
+
+gau_img_tmp = gaupyramid{level};
 mag_tmp = gmag{level};
 dir_tmp = gdir{level};
 x_gmag_tmp = x_gmag{level};
 y_gmag_tmp = y_gmag{level};
+
+gau_img_selected = imcrop(gau_img_tmp, [x-7, y-7, 14, 14]);
 mag_selected = imcrop(mag_tmp, [x-7, y-7, 14, 14]);
 dir_selected = imcrop(dir_tmp, [x-7, y-7, 14, 14]);
 x_gmag_selected = imcrop(x_gmag_tmp, [x-7, y-7, 14, 14]);
@@ -103,46 +115,95 @@ kernel = fspecial('gaussian', [15, 15], 2);
 weighted_mag = kernel.*mag_selected;
 
 figure(4);
-subplot(1, 3, 1);
-imshow(uint8(mag_selected));
+subplot(2, 2, 1);
+
+imshow(uint8(gau_img_selected));
+title('15×15 patch');
+subplot(2, 2, 2);
+imshow(mag_selected, []);
+title('gradient magnitude')
+subplot(2, 2, 3);
+imshow(gau_img_selected, []);
 hold on;
 [X, Y] = meshgrid(1:15, 1:15);
 quiver(X, Y, x_gmag_selected, y_gmag_selected);
-subplot(1, 3, 2);
-imshow(uint8(weighted_mag));
+title('gradient vector visualization');
+subplot(2, 2, 4);
+imshow(weighted_mag, []);
+title('weighted gradient magnitude');
+
+% =========Q5. Orientation histogram for each SIFT key point===========
+hist_raw = zeros(size(keypoint, 1), 39);
+hist_raw(:, 1:3) = keypoint;
+for i=1:size(keypoint, 1)
+    cur_point = keypoint(i, :);
+    level = log2(cur_point(3))+1;
+    mag_tmp = gmag{level};
+    dir_tmp = gdir{level};
+    x = cur_point(1);
+    y = cur_point(2);
+    mag_selected = imcrop(mag_tmp, [x-7, y-7, 14, 14]);
+    %ignore the keypoint which do not have 15*15 window
+    size_tmp = size(mag_selected);
+    if sum(size_tmp) < 30
+        continue;
+    end
+    dir_selected = imcrop(dir_tmp, [x-7, y-7, 14, 14]);
+    w_mag = kernel.*mag_selected;
+    hist_cur = getHistgram(w_mag, dir_selected);
+    hist_raw(i, 4:end) = hist_cur;
+end
+
+
+hist_align = hist_raw;
+for i = 1:size(keypoint, 1)
+    tmp = hist_align(i, 4:end);
+    [~, index] = max(tmp);
+    if index == 1
+        continue;
+    else
+        tmp = [tmp(index:end), tmp(1:index-1)];
+        hist_align(i, 4:end) = tmp;
+    end
+end
+
+% plot the histogram of the selected keypoint
+selected_hist = hist_raw(selected_keypoint_index, 4:end);
+selected_hista = hist_align(selected_keypoint_index, 4:end);
+sltd_keypoint = hist_raw(selected_keypoint_index, 1:3); 
+bin_num = 1:36;
+figure(5);
+subplot(1, 2, 1);
+bar(bin_num, selected_hist);
+title("Histogram without alignment");
+subplot(1, 2, 2);
+bar(bin_num, selected_hista);
+title("Histogram with alignment");
+% =========Q6. Rotate and scale original image===========
+eg1 = imTransform(original_image, 200, 500, 30, 0.5);
+eg2 = imTransform(original_image, 700, 900, -30, 2);
+figure(6)
+subplot(1, 2, 1);
+imshow(uint8(eg1));
+subplot(1, 2, 2);
+imshow(uint8(eg2));
 
 
 
 
-
-
-
-
-% =========================Utils funciton=================================
-function [minv, maxv] = findCurrLocalExtremas(N, x, y, image)
-    minv = 999;
-    maxv = -999;
-    shift = (N-1)/2;
-    for i = x-shift:x+shift
-        for j = y-shift:y+shift
-            if i ~= x && j ~= y
-                if image(i, j) < minv
-                    minv = image(i, j);
-                end
-                if image(i, j) > maxv
-                    maxv = image(i, j);
-                end
-            end
+% =========================Utils funciton================================= 
+function hist = getHistgram(mag, dir)
+    hist = zeros(1, 36);
+    for i = 1:size(mag, 1)
+        for j = 1:size(mag, 2)
+            cur = dir(i, j)+180.0;
+            % index begin from 0
+            index = floor(cur/10);
+            hist(index+1) = hist(index+1) + mag(i, j);
         end
-    end       
+    end         
 end
 
-function [minv, maxv] = getLocalExtremas(N, i, j, image)
-    shift = (N-1)/2;
-    maxv = max(max(image(i-shift:i+shift, j-shift:j+shift)));
-    minv = min(min(image(i-shift:i+shift, j-shift:j+shift)));
-end
-            
 
     
 
